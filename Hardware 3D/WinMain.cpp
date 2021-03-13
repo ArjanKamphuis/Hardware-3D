bec74fc43c96b2d05e5d93081df7f1e4c21db842
ArjanKamphuis <arjan_kamphuis@hotmail.com>
2021-03-13 20:53:58 +0100

    try catch exception handling

--- Hardware 3D/WinMain.cpp
+++ Hardware 3D/WinMain.cpp
@@ -2,15 +2,32 @@
 
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nShowCmd)
 {
-	Window wnd(800, 300, L"Donkey Fart Box");
-
-	MSG msg = {};
-	BOOL bReturn = FALSE;
-	while ((bReturn = GetMessage(&msg, nullptr, 0, 0)) > 0)
+	try
 	{
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
+		Window wnd(800, 300, L"Donkey Fart Box");
+
+		MSG msg = {};
+		BOOL bReturn = FALSE;
+		while ((bReturn = GetMessage(&msg, nullptr, 0, 0)) > 0)
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+
+		return bReturn == -1 ? -1 : static_cast<int>(msg.wParam);
+	}
+	catch (const ChiliException& e)
+	{
+		MessageBoxA(nullptr, e.what(), e.GetType(), MB_OK | MB_ICONEXCLAMATION);
+	}
+	catch (const std::exception& e)
+	{
+		MessageBoxA(nullptr, e.what(), "Standard Exception", MB_OK | MB_ICONEXCLAMATION);
+	}
+	catch (...)
+	{
+		MessageBox(nullptr, L"No details available", L"Unknown Exception", MB_OK | MB_ICONEXCLAMATION);
 	}
 
-	return bReturn == -1 ? -1 : static_cast<int>(msg.wParam);
+	return -1;
 }
